cc28df4d2038cdde925fbb1efd9ec61c43c9af25
ToneDef00 <daniels@dual4t.com>
2020-10-20 04:17:58 -0700

    added functional connection so you can add articles too your reading list.

--- popup.js
+++ popup.js
@@ -18,9 +18,11 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 var db = firebase.firestore();
+var auth = firebase.auth();
+var authApp = auth.app;
 
 
-chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
   let credential = firebase.auth.GoogleAuthProvider.credential(null, token);
   firebase.auth().signInWithCredential(credential);
   console.log("successfull login");
@@ -39,24 +41,51 @@ let trackDomain = document.getElementById('TrackDomain');
 
 
 
-addArticle.addEventListener("click", (event)=>{
-  
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+addArticle.addEventListener("click", (event) => {
+
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     console.log({
-      title:tabs[0].title,
-      url:tabs[0].url,
+      title: tabs[0].title,
+      url: tabs[0].url,
     })
-  //   db.collection("ReadingListItems").add(
-  //     {
-  //       title:tabs[0].title,
-  //       url:tabs[0].url,
-  //     }
-  //   ).then((docRef)=>{
-  //     console.log("document written with ID: "+docRef.id)
-  //   }).catch(function(error) {
-  //     console.error("Error adding document: ", error);
-  // });
+    console.log(auth.currentUser.uid)
+    let User = auth.currentUser;
+    if (User && typeof User.uid === 'string') {
+      //first we check if the article is already in your list
+      db.collection("ViewListItems").where("url", "==", tabs[0].url).where('UID', "==", User.uid).get()
+        .then((querySnapShot) => {
+          // console.log(querySnapShot)
+          if (querySnapShot.empty) {
+            //if the item is not found in your db add it
+            db.collection("ViewListItems").add(
+              {
+                title: tabs[0].title,
+                url: tabs[0].url,
+                UID: User.uid,
+                dateAdded: firebase.firestore.Timestamp.fromDate(new Date()),
+              }
+            ).then((docRef) => {
+              console.log("document written with ID: " + docRef.id)
+            }).catch(function (error) {
+              console.error("Error adding document: ", error);
+            });
+          }
+          else {
+            //handle the document already being found. provide the user feedback
+            console.log(querySnapShot.docs[0].id)
+          }
+
+        }).catch((error) => {
+          console.log("error looking for duplicate records")
+          console.log(error);
+        })
+
+    } else {
+      console.log("no User found or no UID found")
+      console.log(User);
+    }
+
     // use `url` here inside the callback because it's asynchronous!
   });
-  
+
 })
